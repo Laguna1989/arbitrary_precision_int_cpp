0d3f7c6ea590e6e74935f9237fad97d079f70bf6
Simon Weis <laguna.ws@gmail.com>
2023-03-05 07:31:22 +0100

    Avoid superfluous copy in multiplication, add more performance tests

--- performance_tests/performance_test.cpp
+++ performance_tests/performance_test.cpp
@@ -19,3 +19,41 @@ BENCHMARK(BM_Multiplication)
     ->Unit(benchmark::kMillisecond)
     ->ArgsProduct({ benchmark::CreateRange(2, 100, /*multi=*/4),
         benchmark::CreateRange(2, 100, /*multi=*/4) });
+
+static void BM_Addition(benchmark::State& state)
+{
+    std::vector<std::uint8_t> data1;
+    data1.resize(state.range(0), 123u);
+
+    std::vector<std::uint8_t> data2;
+    data2.resize(state.range(1), 123u);
+    api::API const api_1 { data1 };
+    api::API const api_2 { data2 };
+    for (auto _ : state) {
+        auto const result = api_1 + api_2;
+    }
+}
+
+BENCHMARK(BM_Addition)
+    ->Unit(benchmark::kMillisecond)
+    ->ArgsProduct({ benchmark::CreateRange(100, 1600, /*multi=*/4),
+        benchmark::CreateRange(100, 1600, /*multi=*/4) });
+
+static void BM_Division(benchmark::State& state)
+{
+    std::vector<std::uint8_t> data1;
+    data1.resize(state.range(0), 123u);
+
+    std::vector<std::uint8_t> data2;
+    data2.resize(state.range(1), 123u);
+    api::API const api_1 { data1 };
+    api::API const api_2 { data2 };
+    for (auto _ : state) {
+        auto const result = api_1 / api_2;
+    }
+}
+
+BENCHMARK(BM_Division)
+    ->Unit(benchmark::kMillisecond)
+    ->ArgsProduct({ benchmark::CreateRange(2, 100, /*multi=*/4),
+        benchmark::CreateRange(2, 100, /*multi=*/4) });
